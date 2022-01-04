d39e0604a4de6fa9cff668e92435036611d014c2
Volodimir <vova.stepanenko2020@gmail.com>
2022-01-04 20:23:57 +0200

    hashedPwd -> hashedPassword

--- src/main/java/cinema/project/util/HashUtil.java
+++ src/main/java/cinema/project/util/HashUtil.java
@@ -12,15 +12,15 @@ public class HashUtil {
     }
 
     public static String hashPassword(String password, byte[] salt) {
-        StringBuilder hashedPwd = new StringBuilder();
+        StringBuilder hashedPassword = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte element : digest) {
-                hashedPwd.append(String.format("%02x", element));
+                hashedPassword.append(String.format("%02x", element));
             }
-            return hashedPwd.toString();
+            return hashedPassword.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Can`t hash password!", e);
         }
